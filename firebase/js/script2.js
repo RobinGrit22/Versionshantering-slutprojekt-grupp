50e1debcc38c1dcb8dcb653eb25cb5c0fa4089a8
RobinGrit22 <112506404+RobinGrit22@users.noreply.github.com>
2023-01-25 18:10:49 +0100

    Added btns in a form to function

--- firebase/js/script2.js
+++ firebase/js/script2.js
@@ -41,11 +41,12 @@ $('#send').click(function(event){
   $('<p></p>').appendTo(messageItem).text(messageInput)
 
   let messageIconStyle = $('<div>').attr('class', 'messageIconStyle').appendTo(messageItem);
-  $('<button></button>').attr('class', 'likeBtn').appendTo(messageIconStyle)
+  let form = $('<form>').appendTo(messageIconStyle);
+  $('<button></button>').attr('class', 'likeBtn').appendTo(form)
   $(document).ready(function(){
     $(".likeBtn").html('<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>');
 });
-  $('<button></button>').attr('class', 'commentBtn').appendTo(messageIconStyle)
+  $('<button></button>').attr('class', 'commentBtn').appendTo(form)
   $(document).ready(function(){
     $(".commentBtn").html('<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="16 3 21 8 8 21 3 21 3 16 16 3"></polygon></svg>');
 });
